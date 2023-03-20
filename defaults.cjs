0a113eba964b8794e368de84052c726466592c6d
Rajashekarmanda <rajashekarmanda3729@gmail.com>
2023-03-20 13:46:41 +0530

    fixed loop

--- defaults.cjs
+++ defaults.cjs
@@ -1,16 +1,14 @@
 function defaults(obj, defaultProps) {
     if (Array.isArray(obj)) {
         return obj
-    } else if (typeof obj == 'string' || obj == '' || obj == undefined || obj == null || defaultProps==undefined) {
+    } else if (typeof obj == 'string' || obj == '' || obj == undefined || obj == null || defaultProps == undefined) {
         let defaultObject = {}
         for (let eachWord in obj) { defaultObject[eachWord] = obj[eachWord] } return defaultObject
     } else {
-            let defaultObj = defaultProps
-            for (let eachKey in obj) {
-                if (obj[eachKey] !== undefined) {defaultObj[eachKey] = obj[eachKey]
-                } else { defaultObj[eachKey] = defaultProps[eachKey]
-                }
-            }return defaultObj
-        }
+        let defaultObj = defaultProps
+        for (let eachKey in obj) {
+            obj[eachKey] !== undefined ? defaultObj[eachKey] = obj[eachKey] : defaultObj[eachKey] = defaultProps[eachKey]
+        } return defaultObj
     }
+}
 module.exports = defaults
